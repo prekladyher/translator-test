949be1c1c581db17b728aaeeeb63e7b6138746ff
Pavel Horal <pavel.horal@orchitech.cz>
2023-02-19 17:22:23 +0100

    Add LINDAT translation capability.

--- scripts/translate.js
+++ scripts/translate.js
@@ -26,18 +26,62 @@ async function translateDeepl(unit) {
     })
   });
   const data = await response.json();
-  unit.deepl = data.translations?.[0].text;
+  return data.translations?.[0].text;
+}
+
+/**
+ * @param {TranslationUnit} unit
+ */
+async function translateLindat(unit) {
+  const response = await fetch("https://lindat.mff.cuni.cz/services/translation/api/v2/languages/?src=en&tgt=cs", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json"
+    },
+    body: new URLSearchParams({
+      input_text: unit.source
+    })
+  });
+  const data = await response.json();
+  return data?.join(" ").trim();
+}
+
+/**
+ * @type {Record<string, (TranslationUnit) => Promise<string|undefined>}
+ */
+const TRANSLATORS = {
+
+  deepl: translateDeepl,
+
+  lindat: translateLindat
+
+};
+
+/**
+ * @param {string[]} types
+ * @param {TranslationUnit} unit
+ * @param {boolean} reload
+ */
+async function translate(types, unit, reload) {
+  for (const type of types) {
+    if (!reload && unit[type]) {
+      continue; // translation already fetched
+    }
+    unit[type] = await TRANSLATORS[type](unit);
+  }
 }
 
 program
   .name("machine translation")
   .argument("<path>", "translation units JSON")
   .option("-i, --in-place", "add translations to the source file")
-  .option("-t, --type <type>", "translation type", "deepl")
-  .action(async (source, { type, inPlace }) => {
+  .option("-r, --reload", "whether to reload existing translations")
+  .option("-t, --type <type>", "translation type")
+  .action(async (source, { type, inPlace, reload }) => {
     const units = JSON.parse(await readFile(source, "utf-8"));
+    const types = type ? [type] : Object.keys(TRANSLATORS);
     for (const unit of units) {
-      if (!type || type === "deepl") await translateDeepl(unit);
+      await translate(types, unit, reload);
     }
     if (inPlace) {
       await writeFile(source, JSON.stringify(units, null, "  "));
